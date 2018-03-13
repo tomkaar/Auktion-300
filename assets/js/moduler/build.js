3566783cbc5de8febba2e2f6c7f39c0da5168339
Hani Abu Zeid <31956031+swedenSnow@users.noreply.github.com>
2018-03-13 14:30:10 +0100

    Added for-loop for items, added content class in html

--- assets/js/moduler/build.js
+++ assets/js/moduler/build.js
@@ -2,9 +2,25 @@
 async function buildData() {
     
 var auctionAPI = "http://nackowskis.azurewebsites.net/api/Auktion/300";
-
+    
     var data = await api.fetchData(auctionAPI);
     console.log(data);
+
+    var numberInArr = data.length;
+    // console.log(numberinArr);
+    for(let i = 0; i < numberInArr; i++){
+
+        console.log(data[i]);
+       
+        
+        
+    }
+
 }
 
  buildData()
+
+
+
+
+//  api.postData(url, data);
