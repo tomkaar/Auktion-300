d6a5d88224aa2a34d82ba9f29076b6d1c16f278d
roro92 <roro92@users.noreply.github.com>
2018-03-13 16:23:49 +0100

    Createauction

--- assets/js/moduler/createAuction.js
+++ assets/js/moduler/createAuction.js
@@ -1,1 +1,22 @@
+function createAuction() {
+  let Titel = document.getElementById('Titel').value;
+  let Beskrivning = document.getElementById('Beskrivning').value;
+  let Utropspris = document.getElementById('Utropspris').value;
+  let Startdatum = document.getElementById('Startdatum').value;
+  let Slutdatum = document.getElementById('Slutdatum').value;
+  console.log(Titel + Beskrivning + Utropspris + Startdatum + Slutdatum);
+}
+var Skapaauktion = document.getElementById("Skapaauktion");
+Skapaauktion.addEventListener("click",createAuction);
 
+function sendData () {
+
+}
+
+/*
+Titel
+Beskrivning
+Utropspris
+Startdatum
+Slutdatum
+Skapaauktion */
