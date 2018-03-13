8870f7677f6f77d8dba2b9e1072b7289595a3e61
Hani Abu Zeid <31956031+swedenSnow@users.noreply.github.com>
2018-03-13 16:21:26 +0100

    Added content to index.html

--- assets/js/moduler/build.js
+++ assets/js/moduler/build.js
@@ -1,19 +1,34 @@
 
+
 async function buildData() {
     
 var auctionAPI = "http://nackowskis.azurewebsites.net/api/Auktion/300";
     
     var data = await api.fetchData(auctionAPI);
     console.log(data);
-
     var numberInArr = data.length;
     // console.log(numberinArr);
     for(let i = 0; i < numberInArr; i++){
+    console.log(data[i]);
+    
+    let title = document.createElement('h3');
+    title.textContent = data[i].Titel;
+    let description = document.createElement('p');
+    description.textContent = data[i].Beskrivning
+    let startDate = document.createElement('p');
+    startDate.textContent = "Start datum"+ " " + data[i].StartDatum;
+    let endDate = document.createElement('p');
+    endDate.textContent = "Slut datum"+ " " +  data[i].SlutDatum;
+    let startPrice = document.createElement('h3');
+    startPrice.textContent = data[i].Utropspris + ';- sek'
+    
+    
 
-        console.log(data[i]);
-       
-        
-        
+    document.getElementById('content').appendChild(title);     
+    document.getElementById('content').appendChild(description);
+    document.getElementById('content').appendChild(startPrice);      
+    document.getElementById('content').appendChild(startDate);   
+    document.getElementById('content').appendChild(endDate);
     }
 
 }
