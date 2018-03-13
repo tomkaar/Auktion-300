e7f409f0ceeffb18feb88a8b4fa43e185692383c
roro92 <roro92@users.noreply.github.com>
2018-03-13 14:24:50 +0100

    dcs

--- assets/js/moduler/createAuction.js
+++ assets/js/moduler/createAuction.js
@@ -1,10 +1,1 @@
-fetch(url,{
- method: 'POST',
- body: JSON.stringify(minJSONDataFörObjektet),
- headers: {
- 'Accept': 'application/json, text/plain, */*',
- 'Content-Type': 'application/json'
- }
- }).then(function (data) {
- console.log('Request success: ', 'posten skapad');
-})
+
