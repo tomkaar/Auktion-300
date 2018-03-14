4ed2a3388c5367c4ad64a9555a810661f07cf0fa
roro92 <roro92@users.noreply.github.com>
2018-03-14 10:46:50 +0100

    CreateAuction2

--- assets/js/moduler/createAuction.js
+++ assets/js/moduler/createAuction.js
@@ -4,14 +4,27 @@ function createAuction() {
   let Utropspris = document.getElementById('Utropspris').value;
   let Startdatum = document.getElementById('Startdatum').value;
   let Slutdatum = document.getElementById('Slutdatum').value;
+let data =  { "AuktionID": 30,
+"Titel": Titel,
+"Beskrivning": Beskrivning,
+"Utropspris": Utropspris,
+"Startdatum": Startdatum,
+"Slutdatum": Slutdatum,
+"Gruppkod": 300}
+let url = "http://nackowskis.azurewebsites.net/api/auktion/300";
   console.log(Titel + Beskrivning + Utropspris + Startdatum + Slutdatum);
+  console.log(data);
+  console.log(url);
+api.postData(url,data);
 }
+
 var Skapaauktion = document.getElementById("Skapaauktion");
 Skapaauktion.addEventListener("click",createAuction);
 
-function sendData () {
 
-}
+
+
+/*http://nackowskis.azurewebsites.net/api/auktion/300 */
 
 /*
 Titel
