46f9ed3e1fffe2c3a4ac849ee4ef5e141757333b
rooheed <rooheed.gulistani@gmail.com>
2018-03-15 14:51:15 +0100

    Basic style

--- assets/js/moduler/build.js
+++ assets/js/moduler/build.js
@@ -1,18 +1,16 @@
 
 
-async function buildData() {
-    
-    var auctionAPI = "http://nackowskis.azurewebsites.net/api/Auktion/300";
-    
-    var data = await api.fetchData(auctionAPI);
-    
+async function buildData(data) {
+
+    let content = document.getElementById('content');
+    content.innerHTML = "";
 
     var numberInArr = data.length;
     for(let i = 0; i < numberInArr; i++){
-    console.log(data[i]);
+    // console.log(data[i]);
     //Get bids
     let bid = await bud.getBids(data[i].AuktionID)
-    console.log(bid);
+    // console.log(bid);
     //Sort bids
     let higherBid;
     if(bid == 0){
@@ -20,73 +18,104 @@ async function buildData() {
         higherBid = data[i].Utropspris;
 
     } else {
-       
+
         let number = bid.sort((current, val)=> (current.Summa < val.Summa));
-           
+
         higherBid = number[0].Summa;
     };
 
-    console.log(higherBid);
+    // console.log(higherBid);
 
     let auctionsId = data[i].AuktionID;
-    
-
-    let content = document.getElementById('content');
     let container = document.createElement('div');
-    
+
     let title = document.createElement('h3');
     title.textContent = data[i].Titel;
+    title.setAttribute('class', 'title')
     
     let description = document.createElement('p');
     description.textContent = data[i].Beskrivning
+    description.setAttribute('class', 'description');
     
     let startDate = document.createElement('p');
     startDate.textContent = "Start datum"+ " " + data[i].StartDatum;
-    
+    startDate.setAttribute('class', 'startDate')
     let endDate = document.createElement('p');
     endDate.textContent = "Slut datum"+ " " +  data[i].SlutDatum;
+    endDate.setAttribute('class', 'endDate')
+    
     
     let startPrice = document.createElement('h3');
+    startPrice.textContent = data[i].Utropspris + ';- sek';
+    startPrice.setAttribute('class', 'startPrice')
+    
     let currentBid = document.createElement('p');
     currentBid.textContent = higherBid + ";-" + " sek";
+    currentBid.setAttribute('class', 'currentBid');
+    
     let currentHighest = document.createElement('p');
     currentHighest.textContent = "Nuvarande högsta bud";
-    startPrice.textContent = data[i].Utropspris + ';- sek';
-    var bidButton = document.createElement('button');
-    console.log(bidButton);
+    currentHighest.setAttribute('class', 'currentHighest');
+    
+    let bidButton = document.createElement('button');
+    bidButton.textContent = "Placera bud";
+    bidButton.setAttribute('class', 'bidButton');
+    
     let inputBid = document.createElement('input');
     inputBid.setAttribute('type', 'number');
     inputBid.setAttribute('pattern', '^[0-9]+$');
-    
-    
+    inputBid.setAttribute('class', 'inputBid');
+
+
 
     bidButton.textContent = "Placera bud";
-    console.log(bidButton);
+    // console.log(bidButton);
         bidButton.addEventListener("click", function(e){
           let currentInputBid = inputBid.value
           if(currentInputBid == ""){
-            console.log("freakin empty");
+            alert("Du måste ange ett bud");
         }
         else {
 
-            if(currentInputBid >= higherBid){
+            if(currentInputBid > higherBid){
                 bud.newBid(auctionsId, higherBid, currentInputBid)
-                console.log("")
+                alert("Ditt bud är registrerat")
             }
 
             else {
-                console.log("Cheap ass");
-            }
-            
+               alert("Ditt bud är för lågt")
+            } 
+
         }
+  
         });
-    
+       inputBid.addEventListener('keydown', function (keyDown){
+            let name = keyDown.keyCode 
+            if(name == 13){
+                let currentInputBid = inputBid.value
+                if(currentInputBid == ""){
+                  alert("Du måste ange ett bud");
+              }
+              else {
+      
+                  if(currentInputBid > higherBid){
+                      bud.newBid(auctionsId, higherBid, currentInputBid)
+                      alert("Ditt bud är registrerat")
+                  }
+      
+                  else {
+                     alert("Ditt bud är för lågt")
+                  } 
+      
+              }
+        
+            }
+        });
 
-
-    content.appendChild(title);  
-    content.appendChild(description); 
+    content.appendChild(title);
+    content.appendChild(description);
     content.appendChild(startPrice);
-    content.appendChild(currentBid);    
+    content.appendChild(currentBid);
     content.appendChild(currentHighest);
     content.appendChild(currentBid);
     content.appendChild(startDate);
@@ -105,4 +134,4 @@ async function buildData() {
 
 }
 
- buildData()
+
--- assets/js/moduler/search.js
+++ assets/js/moduler/search.js
@@ -1,61 +1,35 @@
+async function search () {
+	var data = await api.fetchData('https://nackowskis.azurewebsites.net/api/Auktion/300');
 
-// få aktuellt datum för att bestämma öppna och stänga auktioner
-		var today = new Date(); // få aktuellt datum
-		var dd = today.getDate(); // få dag
-		var mm = today.getMonth()+1; // få månad januari är 0 som standard
-        var yyyy = today.getFullYear(); // få år
-        
-        // gör dag i två siffror, som standard dag är '1' så gör det 01
-		if(dd<10){
-			dd='0'+dd;
-        } 
-        // gör månad i två siffror, som standard månad är '1' så gör det 01
-		if(mm<10){
-			mm='0'+mm;
-		}		 
-		var today = yyyy+'-'+mm+'-'+dd; // format datum
-		
-var dataSet=[];
-var t;
-$(document).ready(function() {
-    t = $('#example').DataTable(); // skapa data
-} );
+	var sok = document.getElementById('sok').value;
 
-//   function append(parent, el) {
-//     return parent.appendChild(el);
-//   }
+	var selected = document.getElementById("filterSelect").value;
 
-  var i = 0;
-  const dataf = document.getElementById('data');
-  const url = 'https://nackowskis.azurewebsites.net/api/Auktion/300'; // förbereda
+	// filtrera data
+	var filter = data.filter(auktion => auktion.Titel.toUpperCase().includes(sok.toUpperCase()) );
 
-  fetch(url) // hämta data från url
-  .then((resp) => resp.json()) // läs json svar
-  .then(function(data) {
-    let auktions = data; // få json data
 
-    // iterera över data
-    auktions.map(function(auktion) {
-	  dataSet[i] = auktion.AuktionID;
-	  var status = "open";
-	  if(auktion.SlutDatum <= today) // om det stängs
-        status = 'closed';
-        
-        // lägg till rad till data
-	  t.row.add( [
-            auktion.AuktionID,
-            auktion.Titel,
-            auktion.Beskrivning,
-            auktion.StartDatum,
-            auktion.SlutDatum,
-			auktion.Gruppkod,
-			auktion.Utropspris,
-			status
-        ] ).draw( false ); // uppdateringstabell
-		i++;
-    })
-  })
-  // catch error
-  .catch(function(error) {
-    console.log(JSON.stringify(error));
-  });   
+	if(selected =='UtropsPris'){
+		var sort = filter.sort((a,b)=>a.Utropspris > b.Utropspris);
+	} 
+	else {
+			var sort = filter.sort((a,b)=>a.SlutDatum > b.SlutDatum);
+	}
+	
+	if (selected == "Titel"){
+		var sort = filter.sort((a,b)=>a.Titel > b.Titel);
+	}
+
+	buildData(sort);
+
+}
+document.getElementById('click').addEventListener('click', search);
+
+document.getElementById('sok').addEventListener('keydown', function (keyDown){
+	let name = keyDown.keyCode 
+	if(name == 13){
+		search();
+	}
+});
+
+search();
